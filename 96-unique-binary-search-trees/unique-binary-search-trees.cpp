6426991b078050aaedbe11931a464d920a0d2ecd
Sahil Vaidya <131428615+Sahil-Vaidya@users.noreply.github.com>
2025-02-03 08:27:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (6.48%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+int solve(int n, vector<int>& dp){
+    if(n<=1)
+        return 1;
+    if(dp[n]!=-1)
+        return dp[n];
+    int ans=0;
+    //iterate over every root
+    for(int i=1;i<=n;i++){
+        ans += solve(i-1,dp) * solve(n-i,dp);
+    }
+    return dp[n]= ans;
+}
     int numTrees(int n) {
-        if(n<=1)
-            return 1;
-        int ans=0;
-        //iterate over every root
-        for(int i=1;i<=n;i++){
-            ans += numTrees(i-1) * numTrees(n-i);
-        }
-        return ans;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
