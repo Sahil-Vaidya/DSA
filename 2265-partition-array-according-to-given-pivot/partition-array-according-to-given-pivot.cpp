cff19a4f970660a57ddfc788c7692121aa7f20f6
Sahil Vaidya <131428615+Sahil-Vaidya@users.noreply.github.com>
2025-03-03 11:04:43 +0530

    Time: 7 ms (59.22%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less,equal,greater;
 
-        for(int n:nums){
-            if(n<pivot){
-                less.push_back(n);
-            }else if(n == pivot){
-                equal.push_back(n);
-            }else{
-                greater.push_back(n);
-            }
+        int n=nums.size();
+        vector<int> ans(n);
+        int left=0,right=n-1;
+        for(int i=0,j=n-1;i<n;i++,j--){
+            if(nums[i]<pivot)
+                ans[left++] = nums[i];
+            if(nums[j]>pivot)
+                ans[right--]=nums[j];
         }
 
-        //to concatinate
-        vector<int> ans;
-        ans.reserve(nums.size());//ans of same size
-        for(int num:less)  ans.push_back(num);
-        for(int num:equal)  ans.push_back(num);
-        for(int num:greater)   ans.push_back(num);
-
+        while(left<=right){
+            ans[left++]= pivot;
+        }
         return ans;
 
     }
 };
+
+
+
+
+        // vector<int> less,equal,greater;
+
+        // for(int n:nums){
+        //     if(n<pivot){
+        //         less.push_back(n);
+        //     }else if(n == pivot){
+        //         equal.push_back(n);
+        //     }else{
+        //         greater.push_back(n);
+        //     }
+        // }
+
+        // //to concatinate
+        // vector<int> ans;
+        // ans.reserve(nums.size());//ans of same size
+        // for(int num:less)  ans.push_back(num);
+        // for(int num:equal)  ans.push_back(num);
+        // for(int num:greater)   ans.push_back(num);
+
+        // return ans;
+
+//     }
+// };
