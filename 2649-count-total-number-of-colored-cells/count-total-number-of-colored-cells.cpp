f0000b90093bad0bc00ae1fce1d8fff5e71c83e8
Sahil Vaidya <131428615+Sahil-Vaidya@users.noreply.github.com>
2025-03-05 08:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.17%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long ans = 1;
+        long long val = 4;
+        if(n==1)  return 1;
+
+        for(int i=2;i<=n;i++){
+            ans +=val;
+            val = 4 * i;
+        }
+        return ans;
+    }
+};
