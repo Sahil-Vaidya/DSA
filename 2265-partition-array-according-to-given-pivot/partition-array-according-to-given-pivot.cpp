e12a1ecca896e55205564d192f9d5ccee52e1e40
Sahil Vaidya <131428615+Sahil-Vaidya@users.noreply.github.com>
2026-06-08 09:00:53 +0530

    Time: 4 ms (81.51%) | Memory: 127.7 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-
         int n=nums.size();
-        vector<int> ans(n);
         int left=0,right=n-1;
-        for(int i=0,j=n-1;i<n;i++,j--){
-            if(nums[i]<pivot)
-                ans[left++] = nums[i];
-            if(nums[j]>pivot)
-                ans[right--]=nums[j];
+        vector<int> ans(n);
+        //left
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                ans[left++]=nums[i];
+            }
         }
-
+        //right
+        for(int i=n-1;i>=0;i--){
+            if(pivot<nums[i])
+                ans[right--]=nums[i];
+        }
+        //pivot
         while(left<=right){
-            ans[left++]= pivot;
+            ans[left++]=pivot;
         }
         return ans;
 
     }
 };
-
-
-
-
-        // vector<int> less,equal,greater;
-
-        // for(int n:nums){
-        //     if(n<pivot){
-        //         less.push_back(n);
-        //     }else if(n == pivot){
-        //         equal.push_back(n);
-        //     }else{
-        //         greater.push_back(n);
-        //     }
-        // }
-
-        // //to concatinate
-        // vector<int> ans;
-        // ans.reserve(nums.size());//ans of same size
-        // for(int num:less)  ans.push_back(num);
-        // for(int num:equal)  ans.push_back(num);
-        // for(int num:greater)   ans.push_back(num);
-
-        // return ans;
-
-//     }
-// };
